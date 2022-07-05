6c132734ca9f6266467225a7536b6eacb1f2ef6a
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-07-05 13:08:17 -0300

    Update and rename Ejercicio 2: Funciones, definición to Ejercicio 2: Funciones, definición.js

---
+++ 2. Programación Imperativa/1. Funciones y tipos de datos/Ejercicio 2: Funciones, definición.js
@@ -0,0 +1,30 @@
+/*
+Gobstones y JavaScript tienen mucho en común. Por ejemplo, en ambos lenguajes podemos definir funciones y usarlas muchas veces. 
+Sin embargo, como siempre que aprendas un lenguaje nuevo, te vas a topar con un pequeño detalle: tiene una sintaxis diferente . La buena noticia es que el cambio no será tan terrible como suena, así que veamos nuestra primera función JavaScript: 
+function doble(numero) {
+   return 2 * numero;
+}
+ CopiarDiferente, pero no tanto. Si la comparás con su equivalente Gobstones...
+function doble(numero) {
+  return (2* numero)
+}
+ Copiar...notarás que los paréntesis en el return no son necesarios, y que la última línea la terminamos con ;.
+
+
+Veamos si se va entendiendo: definí ahora la función mitad, que tome un número por parámetro y retorne su mitad. Tené en cuenta que el operador de división en JavaScript es /.
+*/
+
+function mitad(num){
+  return num / 2;
+}
+
+/*
+ ¡Muy bien! Tu solución pasó todas las pruebas
+Perfecto, ¿viste que no era tan terrible?
+
+Si no le pusiste ; al final de la sentencia habrás visto que funciona igual. De todas formas ponelo, ya que de esa manera evitamos posibles problemas.
+
+Siempre que aprendamos un lenguaje nuevo vamos a tener que aprender una nueva sintaxis. Sin embargo y por fortuna, si tenés los conceptos claros, no es nada del otro mundo .
+
+Aprendamos ahora a usar estas funciones
+*/
