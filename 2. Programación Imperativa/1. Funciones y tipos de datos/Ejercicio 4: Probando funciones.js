dc8dd493d118c5c67dd86787c4ce5ba02392fe07
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-07-06 15:32:25 -0300

    Create Ejercicio 4: Probando funciones.js

---
+++ 2. Programación Imperativa/1. Funciones y tipos de datos/Ejercicio 4: Probando funciones.js
@@ -0,0 +1,15 @@
+/*
+Quizás ya lo notaste pero, junto al editor, ahora aparece una pestaña nueva: la consola.
+
+La consola es una herramienta muy útil para hacer pruebas rápidas sobre lo que estás haciendo: te permite, por ejemplo, probar expresiones, funciones que vengan con JavaScript, o incluso funciones que vos definas en el editor.
+
+La podés reconocer fácilmente porque arranca con un chirimbolito que se llama prompt.
+
+Para entender mejor cómo funciona, te invitamos a explorarla.
+
+Probá en la consola las siguientes expresiones:
+
+4 + 5
+Math.round(4.5)
+funcionMisteriosa(1, 2, 3) (ya la definimos por vos y la podés usar)
+*/
