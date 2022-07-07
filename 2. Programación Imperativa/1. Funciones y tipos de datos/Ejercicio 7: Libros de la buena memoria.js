1da4d1eb2e7f412138e922bbdb4457466601d0fd
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-07-06 21:53:26 -0300

    Create Ejercicio 7: Libros de la buena memoria.js

---
+++ 2. Programación Imperativa/1. Funciones y tipos de datos/Ejercicio 7: Libros de la buena memoria.js
@@ -0,0 +1,23 @@
+//¡Veamos más operadores! Dani ama el primer día de cada mes , y por eso definió esta función...
+
+function esDiaFavorito(diaDelMes) {
+  return diaDelMes === 1 ;
+}
+//...y la usa así (y la dejó en la biblioteca para que la pruebes):
+
+> esDiaFavorito(13)
+false
+> esDiaFavorito(1)
+true
+
+//Como ves, en JavaScript contamos con operadores como ===, >=, >, <,<= que nos dicen si dos valores son iguales, mayores-o-iguales, mayores, etc. Los vamos a usar bastante .
+
+//¡Ahora te toca a vos! Dani también dice que a alguien leGustaLeer, cuando la cantidad de libros que recuerda haber leído es mayor a 20. Por ejemplo:
+
+> leGustaLeer(15)
+false
+
+> leGustaLeer(45)
+true
+//Definí y probá en la consola la función leGustaLeer.
+
