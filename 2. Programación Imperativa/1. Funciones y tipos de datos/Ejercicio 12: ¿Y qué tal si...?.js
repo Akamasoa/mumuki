5ed92b7e27a1d1bd5112f0e257498790629618a9
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-07-08 13:29:18 -0300

    Update Ejercicio 12: ¿Y qué tal si...?.js

--- 2. Programación Imperativa/1. Funciones y tipos de datos/Ejercicio 12: ¿Y qué tal si...?.js
+++ 2. Programación Imperativa/1. Funciones y tipos de datos/Ejercicio 12: ¿Y qué tal si...?.js
@@ -12,3 +12,12 @@ function valorAbsoluto(unNumero) {
 
 //Veamos si se entiende: definí la función maximo, que funcione como Math.max (¡no vale usarla!) y retorne el máximo entre dos números. Por ejemplo, el máximo entre 4 y 5 es 5, y el máximo entre 10 y 4, es 10.
 
+function maximo(num1, num2){
+  if (num1 > num2){
+    return num1;
+  } else {
+    return num2;
+  }
+}
+
+//¡Muy bien! Tu solución pasó todas las pruebas
