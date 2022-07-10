332723a766927fc8a66f6645536148ca8cfcf9a2
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-07-09 21:54:55 -0300

    Update Ejercicio 13: ¿De qué signo sos?.js

--- 2. Programación Imperativa/1. Funciones y tipos de datos/Ejercicio 13: ¿De qué signo sos?.js
+++ 2. Programación Imperativa/1. Funciones y tipos de datos/Ejercicio 13: ¿De qué signo sos?.js
@@ -20,3 +20,14 @@ function cuidadoSegun(dia) {
 0 si el número es cero
 -1 si el número es negativo
 
+function signo (num){
+  if (num > 0){
+    return 1;
+  }else if (num < 0){
+    return -1;
+  }else{
+    return 0;
+  }
+}
+
+//¡Muy bien! Tu solución pasó todas las pruebas
