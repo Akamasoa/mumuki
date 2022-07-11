c4b4a1469d5de368974ab6cd981d505b7a7f4e9f
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-07-10 23:55:09 -0300

    Create Ejercicio 17: Datos de todo tipo.js

---
+++ 2. Programación Imperativa/1. Funciones y tipos de datos/Ejercicio 17: Datos de todo tipo.js
@@ -0,0 +1,11 @@
+/*
+Uff, ¡vimos un montón de cosas! Aprendimos sobre la sintaxis de las funciones en JavaScript, los tipos de datos y sus operaciones, e incluso conocimos uno nuevo: los strings.
+
+¡Para finalizar veamos algunos ejemplos!
+
+4 + 4 vale 8.
+"4" + "4" vale "44".
+"on" + "ce" vale "once".
+true && false vale false.
+5 >= 6 vale false.
+*/
