873f75be632af4e75dc0b98f5bc8c15de589a2e8
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-07-12 13:06:33 -0300

    Update Ejercicio 4: Cuadrados.js

--- 2. Programación Imperativa/2. Práctica Funciones y Tipos de Datos/Ejercicio 4: Cuadrados.js
+++ 2. Programación Imperativa/2. Práctica Funciones y Tipos de Datos/Ejercicio 4: Cuadrados.js
@@ -7,3 +7,12 @@ areaCuadrado: dado un lado, queremos saber cuál es su area
 Definí las funciones perimetroCuadrado y areaCuadrado
 */
 
+function perimetroCuadrado (lado){
+  return lado * 4;
+}
+
+function areaCuadrado(lado){
+  return lado * lado;
+}
+
+// ¡Muy bien! Tu solución pasó todas las pruebas
