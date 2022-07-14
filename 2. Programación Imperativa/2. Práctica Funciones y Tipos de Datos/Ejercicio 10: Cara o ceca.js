87eca20e7b8354649abf0e0e46bddeaecd1e7a11
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-07-14 13:34:48 -0300

    Create Ejercicio 10: Cara o ceca.js

---
+++ 2. Programación Imperativa/2. Práctica Funciones y Tipos de Datos/Ejercicio 10: Cara o ceca.js
@@ -0,0 +1,11 @@
+/*
+Hay veces en las que tenemos difíciles decisiones que tomar en nuestras vidas (como por ejemplo, si comer pizzas o empanadas ), y no tenemos más remedio que dejarlas libradas a la suerte.
+
+Es allí que tomamos una moneda y decimos: si sale cara, comemos pizzas, si no, empanadas.
+
+Definí una función decisionConMoneda, que toma tres parámetros y retorna el segundo si el primero es "cara", o el tercero, si sale "ceca". Por ejemplo:
+*/
+
+> decisionConMoneda("cara", "pizzas", "empanadas")
+"pizzas"
+
