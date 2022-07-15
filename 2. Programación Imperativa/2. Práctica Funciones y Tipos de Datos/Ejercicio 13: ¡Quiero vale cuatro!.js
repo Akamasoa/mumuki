090bd2c28a479824ee1a4dc4c2a8c330e5494b59
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-07-15 19:56:12 -0300

    Update Ejercicio 13: ¡Quiero vale cuatro!.js

--- 2. Programación Imperativa/2. Práctica Funciones y Tipos de Datos/Ejercicio 13: ¡Quiero vale cuatro!.js
+++ 2. Programación Imperativa/2. Práctica Funciones y Tipos de Datos/Ejercicio 13: ¡Quiero vale cuatro!.js
@@ -13,3 +13,14 @@ Definí la función valorCantoTruco, que tome el canto y retorne cuántos puntos
 
 //Asumí que sólo te van a pasar como argumento un string que represente un canto de truco. Por ejemplo, no vamos a probar la función para el caso valorCantoTruco("zaraza")
 
+function valorCantoTruco(canto){
+  if (canto === "truco"){
+    return 2;
+  } else if(canto === "retruco"){
+    return 3;
+  } else {
+    return 4;
+  }
+}
+
+// ¡Muy bien! Tu solución pasó todas las pruebas
