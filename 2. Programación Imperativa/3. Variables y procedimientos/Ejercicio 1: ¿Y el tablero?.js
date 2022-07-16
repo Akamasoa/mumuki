1b1f2c50a64ce7742864a13ac37f9b6f3e746b13
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-07-16 20:23:45 -0300

    Create Ejercicio 1: ¿Y el tablero?.js

---
+++ 2. Programación Imperativa/3. Variables y procedimientos/Ejercicio 1: ¿Y el tablero?.js
@@ -0,0 +1,14 @@
+/*
+Hasta ahora en esta película hay un gran personaje que está faltando: el tablero. Seguro está por aparecer, de forma triunfal y rimbombante..., ¿no?
+
+No. En JavaScript, lamentamos informarte, no hay tablero .
+
+Pero tampoco es tan grave: en JavaScript no hay tablero, ¡porque no lo necesitás! Suceden dos cosas:
+
+El tablero nos servía para ver lo que nuestro programa hacía y qué resultados generaba. Nos permitía también observar los cambios de estado a causa del programa. Pero ahora ya tenemos experiencia suficiente como para lanzarnos a programar sin tener que "ver" lo que sucede.
+Ahora contamos con la consola: una herramienta poderosa que nos permite hacer pruebas más detalladas y flexibles.
+¿No nos creés? Te presentamos un desafío: usando la consola, decí con tus propias palabras qué hace la función funcionMisteriosa, que recibe dos números enteros como argumentos.
+
+¡Vas a ver que podés averiguarlo sin tener un tablero!
+*/
+
