2c51329b4ee2d656d9ba4b18d7c9fce4ddc66d91
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-07-21 23:07:08 -0300

    Update Ejercicio 2: Negar no cuesta nada.js

--- 2. Programación Imperativa/4. Lógica booleana/Ejercicio 2: Negar no cuesta nada.js
+++ 2. Programación Imperativa/4. Lógica booleana/Ejercicio 2: Negar no cuesta nada.js
@@ -8,3 +8,15 @@ function esImpar(numero) {
 
 //¡Ahora te toca a vos! Definí esMayorDeEdad, que recibe una edad, y luego esMenorDeEdad a partir de ella.
 
+function esMayorDeEdad(edad){
+  return edad >= 18;
+}
+function esMenorDeEdad(edad){
+  return !esMayorDeEdad(edad);
+}
+
+// ¡Muy bien! Tu solución pasó todas las pruebas
+
+//Cada una de las funciones representa un estado de dos posibles: ser mayor o ser menor de edad. No se puede ser ambos al mismo tiempo y tampoco se puede evitar pertenecer a alguno de los dos grupos. Es decir, ¡siempre sos uno u otro! 
+
+//Por eso decimos que son complementarios y que juntos forman el conjunto universal.
