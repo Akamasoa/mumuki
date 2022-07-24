91edd04692f9c0eb690a453ea9d10ddccef9ea3d
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-07-23 22:55:12 -0300

    Create Ejercicio 9: ¡Hola! Mi nombre es Xor.js

---
+++ 2. Programación Imperativa/4. Lógica booleana/Ejercicio 9: ¡Hola! Mi nombre es Xor.js
@@ -0,0 +1,14 @@
+/*Ahora cambiemos las proposiciones la letra tiene tono claro y el fondo tiene tono claro por proposiciones genéricas A y B. Además, representemos la operación que realiza tiene contraste con el símbolo ⊻. Lo que obtenemos es... ¡una nueva tabla! 
+
+A	B	A ⊻ B
+V	V	F
+V	F	V
+F	V	V
+F	F	F
+Este comportamiento existe como un operador dentro de la lógica y se lo denomina xor o disyunción lógica excluyente.
+
+A diferencia del and, or y not, el xor no suele estar definido en los lenguajes.  Sin embargo, ahora que sabés cómo funciona, si alguna vez lo necesitás podés definirlo a mano. 
+
+Veamos si se entiende: definí la función genérica xor, que tome dos booleanos y retorne el valor de verdad correspondiente.
+*/
+
