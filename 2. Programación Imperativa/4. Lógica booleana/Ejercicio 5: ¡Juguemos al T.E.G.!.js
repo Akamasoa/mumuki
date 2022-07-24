acf78c1357f02b8555252f019980d53510465e3e
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-07-23 22:49:55 -0300

    Update Ejercicio 5: ¡Juguemos al T.E.G.!.js

--- 2. Programación Imperativa/4. Lógica booleana/Ejercicio 5: ¡Juguemos al T.E.G.!.js
+++ 2. Programación Imperativa/4. Lógica booleana/Ejercicio 5: ¡Juguemos al T.E.G.!.js
@@ -9,3 +9,12 @@ function gano(cumplioObjetivoSecreto, cantidadDePaisesConquistados) {
   return cumplioObjetivoSecreto || cantidadDePaisesConquistados >= 30;
 }
 
+//Probá en la consola las siguientes expresiones:
+
+ gano(true, 25)
+ gano(false, 30)
+ gano(false, 20)
+ gano(true, 31)
+
+//¿Te animás a construir la tabla de verdad de la disyunción lógica?
+
