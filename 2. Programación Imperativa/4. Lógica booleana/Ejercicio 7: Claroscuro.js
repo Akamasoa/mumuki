ca2af856c71f0cfd780a9f283e13e18ad3235516
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-07-23 22:52:48 -0300

    Update Ejercicio 7: Claroscuro.js

--- 2. Programación Imperativa/4. Lógica booleana/Ejercicio 7: Claroscuro.js
+++ 2. Programación Imperativa/4. Lógica booleana/Ejercicio 7: Claroscuro.js
@@ -10,3 +10,8 @@ true
 
 //¡Ahora te toca a vos! Definí la función tieneContraste. Para que la página tenga contraste tiene que tener el fondo claro y la letra no o bien tener la letra clara y el fondo no.
 
+function tieneContraste (colorletra,colorfondo){
+  return (!esTonoClaro(colorletra)&&esTonoClaro(colorfondo))||(esTonoClaro(colorletra)&&!esTonoClaro(colorfondo));
+}
+
+// ¡Muy bien! Tu solución pasó todas las pruebas
