3d1c7831e7b3a5aa35c1db7c9a6653de17b6674b
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-07-26 22:43:32 -0300

    Create Ejercicio 11: Un ejercicio sin precedentes.js

---
+++ 2. Programación Imperativa/4. Lógica booleana/Ejercicio 11: Un ejercicio sin precedentes.js
@@ -0,0 +1,21 @@
+/*
+Si prestaste atención a la función anterior, habrás notado que la operación con mayor precedencia es la negación !, seguida de la conjunción && y por último la disyunción ||. ¿Pero qué pasa si quiero alterar el orden en que se resuelven?
+
+Al igual que en matemática, podemos usar paréntesis para agrupar las operaciones que queremos que se realicen primero.
+
+Delfi se puede concentrar cuando programa y toma infusiones, pero no cualquier infusión. Tiene que ser mate a exactamente 80ºC o té que esté a por lo menos 95ºC.
+
+Definí la función sePuedeConcentrar que recibe una bebida, su temperatura y un booleano que nos dice si Delfi está programando:
+*/
+
+> sePuedeConcentrar('té', 100, true)
+true
+
+> sePuedeConcentrar('mate', 70, true)
+false
+
+> sePuedeConcentrar('té', 95, false)
+false
+
+//¡Intentá resolverlo en una única función! Después vamos a ver cómo quedaría si delegamos.
+
