b6f851a7560ed8c77393d3fada32b3900231372e
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-08-05 23:06:13 -0300

    Create Ejercicio 9: Contiene.js

---
+++ 2. Programación Imperativa/5. Listas/Ejercicio 9: Contiene.js
@@ -0,0 +1,13 @@
+//¡Ahora te toca a vos!
+
+//Definí la función contiene que nos diga si una lista contiene un cierto elemento.
+
+ contiene([1, 6, 7, 6], 7)
+true
+ contiene([1, 6, 7, 6], 6)
+true
+ contiene([], 7)
+false
+ contiene([8, 5], 7)
+false
+
