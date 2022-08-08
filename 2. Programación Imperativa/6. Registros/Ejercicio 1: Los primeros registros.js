5226594d2bd984e316212984cd8d68ba1f7e9534
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-08-07 22:09:28 -0300

    Create Ejercicio 1: Los primeros registros.js

---
+++ 2. Programación Imperativa/6. Registros/Ejercicio 1: Los primeros registros.js
@@ -0,0 +1,20 @@
+/*Una historiadora está recopilando información acerca de distintos monumentos a lo largo y ancho del mundo . En principio solo quiso saber el nombre, ubicación, y año de construcción de cada monumento.
+
+Para eso almacenó cada dato en una variable:
+
+nombreEstatuaDeLaLibertad = "Estatua de la Libertad";
+locacionEstatuaDeLaLibertad = "Nueva York";
+anioDeConstruccionEstatuaDeLaLibertad = "1886";
+nombreCristoRedentor = "Cristo Redentor";
+locacionCristoRedentor = "Rio De Janeiro";
+anioDeConstruccionCristoRedentor = "1931";
+Ahí es cuando se dio cuenta que no era conveniente : si bien la información entre las variables estaba relacionada, la estaba almacenando por separado. Entonces pensó: ¿no existirá alguna forma de representar las distintas características o propiedades de una misma cosa de forma agrupada?
+
+Luego de investigar un poco, encontró una mejor manera para guardar la información de los monumentos. Probá en la consola escribiendo:
+
+estatuaDeLaLibertad
+cristoRedentor
+torreEiffel
+tajMahal
+coliseo
+*/
