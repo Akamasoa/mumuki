cd517305808d77951f2b7be23b9cd6a07b093b5e
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-08-08 21:51:03 -0300

    Create Ejercicio 4: Temperatura de planeta.js

---
+++ 2. Programación Imperativa/6. Registros/Ejercicio 4: Temperatura de planeta.js
@@ -0,0 +1,11 @@
+//Ahora que agregamos registros de planetas, ¡trabajemos un poco con ellos!
+
+//Definí una función temperaturaDePlaneta que reciba como argumento un registro de planeta y retorne un string que indique su nombre y su temperatura promedio. ¡Tiene que funcionar para cualquier planeta! Por ejemplo:
+
+> temperaturaDePlaneta(mercurio)
+"Mercurio tiene una temperatura promedio de 67 grados"
+> temperaturaDePlaneta(saturno)
+"Saturno tiene una temperatura promedio de -139 grados"
+> temperaturaDePlaneta(venus)
+"Venus tiene una temperatura promedio de 462 grados"
+
