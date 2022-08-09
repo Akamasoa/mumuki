94c7d14b4e69a2864129cdddb273ef8998158847
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-08-08 21:48:58 -0300

    Create Ejercicio 2: Tu propio monumento.js

---
+++ 2. Programación Imperativa/6. Registros/Ejercicio 2: Tu propio monumento.js
@@ -0,0 +1,10 @@
+/*
+Los monumentos que probaste en el ejercicio anterior están representados como registros, y cada una de sus características (nombre, locación, año de construcción) son campos del registro. Por cierto, ¡podemos crear registros de cualquier cosa, con los campos que querramos!
+
+Por ejemplo, podríamos almacenar un libro de modo que cada campo del registro fuese alguna característica: su título, su autor, su fecha de publicación, y más.
+
+¡Es tu momento del monumento! Guardá en las variables torreAzadi y monumentoNacionalALaBandera registros de esos monumentos, oriundos de las ciudades de Teherán, Irán y Rosario, Argentina respectivamente. ¿Te animás a investigar en qué año se terminaron de construir para completar ese campo?
+*/
+
+
+ 
