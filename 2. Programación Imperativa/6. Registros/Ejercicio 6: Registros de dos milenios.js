4923937b6126651c26105c5b2e3604a68828ec04
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-08-08 21:54:37 -0300

    Create Ejercicio 6: Registros de dos milenios.js

---
+++ 2. Programación Imperativa/6. Registros/Ejercicio 6: Registros de dos milenios.js
@@ -0,0 +1,7 @@
+//En el ejercicio anterior modificamos la ruta del registro, pero no utilizamos su fecha de creación. ¡Usémosla! Queremos saber si un archivo es del milenio pasado, lo que ocurre cuando su año es anterior a 2000.
+
+//Definí la función esDelMilenioPasado, que recibe un archivo y retorna un booleano.
+
+> esDelMilenioPasado({ ruta: "D:\fotonacimiento.jpg", creacion: "14/09/1989" })
+true
+
