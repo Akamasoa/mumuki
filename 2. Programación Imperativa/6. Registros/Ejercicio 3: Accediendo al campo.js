a16bf89643c83bbd985568916c457c7bc2d8bce3
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-08-08 21:50:28 -0300

    Create Ejercicio 3: Accediendo al campo.js

---
+++ 2. Programación Imperativa/6. Registros/Ejercicio 3: Accediendo al campo.js
@@ -0,0 +1,14 @@
+//Cuando consultaste los registros existentes, se veía algo parecido a lo siguiente:
+
+> tajMahal
+{ nombre: "Taj Mahal", locacion: "Agra, India", anioDeConstruccion: 1653 }
+
+//Esa consulta era porque estábamos viendo al registro tajMahal completo, incluyendo todos sus campos. ¡Pero también se puede consultar por un campo particular! Mirá :
+
+> tajMahal.locacion
+"Agra, India"
+> tajMahal.anioDeConstruccion
+1653
+
+//Declaramos los planetas mercurio, marte y saturno como registros con la siguiente información: nombre, temperaturaPromedio y si tieneAnillos. ¡Probalos en la consola!
+
