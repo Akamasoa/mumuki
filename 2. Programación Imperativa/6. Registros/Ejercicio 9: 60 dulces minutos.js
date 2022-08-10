edbc343af84809bb5b401a2575d8a812581f57e6
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-08-10 15:00:05 -0300

    Update and rename Workout 9: 60 dulces minutos.js to Ejercicio 9: 60 dulces minutos.js

--- 2. Programación Imperativa/6. Registros/Ejercicio 9: 60 dulces minutos.js
+++ 2. Programación Imperativa/6. Registros/Ejercicio 9: 60 dulces minutos.js
@@ -3,3 +3,8 @@
 
 //Definí el procedimiento agregarAPostresRapidos, que toma una lista con postres rápidos y un postre por parámetro. Si el tiempo de cocción es de una hora o menos, se agrega el registro a la lista.
 
+function agregarAPostresRapidos(lisPost,regPostre){
+  if (regPostre.tiempoDeCoccion <= 60){
+    agregar(lisPost, regPostre);
+  }
+}
