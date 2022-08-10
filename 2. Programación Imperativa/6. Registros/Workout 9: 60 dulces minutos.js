29fa4564a17e773e64e50ad21dc2cf6acb3bdad5
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-08-10 14:59:08 -0300

    Create Workout 9: 60 dulces minutos.js

---
+++ 2. Programación Imperativa/6. Registros/Workout 9: 60 dulces minutos.js
@@ -0,0 +1,5 @@
+//A veces no sólo queremos comer algo rico, sino que queremos comerlo lo antes posible.  
+
+
+//Definí el procedimiento agregarAPostresRapidos, que toma una lista con postres rápidos y un postre por parámetro. Si el tiempo de cocción es de una hora o menos, se agrega el registro a la lista.
+
