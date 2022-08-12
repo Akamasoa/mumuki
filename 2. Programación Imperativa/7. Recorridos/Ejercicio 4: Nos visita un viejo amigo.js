0b11397f4eb5da4d8ba2647f37cb703fd5bc051c
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-08-11 22:41:12 -0300

    Update Ejercicio 4: Nos visita un viejo amigo.js

--- 2. Programación Imperativa/7. Recorridos/Ejercicio 4: Nos visita un viejo amigo.js
+++ 2. Programación Imperativa/7. Recorridos/Ejercicio 4: Nos visita un viejo amigo.js
@@ -12,3 +12,24 @@ function gananciaTotal(balancesDeUnPeriodo) {
 
 //¿Aún no te convenciste? Nuevamente, probá las siguientes expresiones en la consola:
 
+> gananciaTotal([])
+> gananciaTotal([
+    { mes: "noviembre", ganancia: 5 }
+   ])
+> gananciaTotal([
+    { mes: "marzo", ganancia: 8 }, 
+    { mes: "agosto", ganancia: 10 }
+   ])
+> gananciaTotal([
+    { mes: "enero", ganancia: 2 }, 
+    { mes: "febrero", ganancia: 10 }, 
+    { mes: "marzo", ganancia: -20 }
+   ])
+> gananciaTotal([
+    { mes: "enero", ganancia: 2 }, 
+    { mes: "febrero", ganancia: 10 }, 
+    { mes: "marzo", ganancia: -20 }, 
+    { mes: "abril", ganancia: 0 }, 
+    { mes: "mayo", ganancia: 10 }
+   ])
+
