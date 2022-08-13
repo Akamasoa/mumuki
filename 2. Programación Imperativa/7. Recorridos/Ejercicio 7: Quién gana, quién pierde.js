12310b29369384699a6515c4a579983952b98038
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-08-13 20:54:46 -0300

    Create Ejercicio 7: Quién gana, quién pierde.js

---
+++ 2. Programación Imperativa/7. Recorridos/Ejercicio 7: Quién gana, quién pierde.js
@@ -0,0 +1,9 @@
+/*
+Viendo que podemos hacer todo lo que nos pide, Ana quiere saber la ganancia promedio de los balances positivos.
+
+Definí las funciones:
+
+gananciaPositiva, que es la suma de las ganancias de los balances positivos
+promedioGananciasPositivas invocando gananciaPositiva y cantidadDeBalancesPositivos.
+*/
+
