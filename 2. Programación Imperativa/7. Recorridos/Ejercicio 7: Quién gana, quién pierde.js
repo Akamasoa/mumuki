05d15b0df34cba16081e9c6fc32167a29a409b1e
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-08-13 20:55:56 -0300

    Update Ejercicio 7: Quién gana, quién pierde.js

--- 2. Programación Imperativa/7. Recorridos/Ejercicio 7: Quién gana, quién pierde.js
+++ 2. Programación Imperativa/7. Recorridos/Ejercicio 7: Quién gana, quién pierde.js
@@ -7,3 +7,16 @@ gananciaPositiva, que es la suma de las ganancias de los balances positivos
 promedioGananciasPositivas invocando gananciaPositiva y cantidadDeBalancesPositivos.
 */
 
+function gananciaPositiva(balancesDeUnPeriodo){
+  let sumatoria = 0;
+  for (let balance of balancesDeUnPeriodo){
+    if (balance.ganancia > 0){
+      sumatoria = sumatoria + balance.ganancia;
+    }
+  }
+  return sumatoria;
+}
+
+function promedioGananciasPositivas(balancesDeUnPeriodo){
+  return (gananciaPositiva(balancesDeUnPeriodo) / cantidadDeBalancesPositivos(balancesDeUnPeriodo));
+}
