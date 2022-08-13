6abaaebaa3becb4283c681adf238cdca1c9400cc
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-08-12 23:22:17 -0300

    Create Ejercicio 6: La ganancia promedio.js

---
+++ 2. Programación Imperativa/7. Recorridos/Ejercicio 6: La ganancia promedio.js
@@ -0,0 +1,10 @@
+//Pasemos al siguiente requerimiento de Ana. Ya podemos calcular una sumatoria de ganancias y también crear contadores, ahora vamos a calcular promedios.
+
+//Ana quisiera saber dado un conjunto cualquiera de balances cuál es su gananciaPromedio.
+
+> gananciaPromedio([
+      { mes: "marzo", ganancia: 8 }, 
+      { mes: "agosto", ganancia: 10 }
+  ])
+9
+
