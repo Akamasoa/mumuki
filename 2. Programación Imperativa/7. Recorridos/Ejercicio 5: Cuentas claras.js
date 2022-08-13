1e96a236018da705235ddc0916ad5d58b03d7d58
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-08-12 23:20:59 -0300

    Create Ejercicio 5: Cuentas claras.js

---
+++ 2. Programación Imperativa/7. Recorridos/Ejercicio 5: Cuentas claras.js
@@ -0,0 +1,4 @@
+//¡Ana tiene nuevos requirimientos! Ahora nos pidió lo siguiente: "Quiero saber cuántos balances fueron positivos, es decir, aquellos en los que la ganancia fue mayor a cero".
+
+//Completá la función cantidadDeBalancesPositivos. Si prestás atención, notarás que tiene una estructura similar al problema anterior.
+
